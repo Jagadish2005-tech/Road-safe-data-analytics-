a6a7e1ce9ef11248057efd6eeddf9b48f5b17838
Jagadish2005-tech <128226576+Jagadish2005-tech@users.noreply.github.com>
2025-10-06 22:46:02 +0530

    Add files via upload

---
+++ Milestone_3/Day_17/app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+# Set the title of the Streamlit app
+st.title('Hello from Streamlit in Docker!')
+
+# Write a simple message to the app
+st.write('This is a simple web app running inside a Docker container.')
+
+# You can add more Streamlit components here
+st.balloons()
